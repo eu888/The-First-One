365286e220a188c592affa23c13c0024f0005efd
Me88_88 <68464396+eu888@users.noreply.github.com>
2025-05-17 23:11:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from discord.ext import commands
+from keep_alive import keep_alive
 
 GUILD_ID = 881250112549027880  
 
@@ -66,4 +67,7 @@ async def help(interaction: discord.Interaction):
     embed.add_field(name="/test [arg1] [arg2]", value="Returns your input to test argument parsing.", inline=False)
     embed.add_field(name="/help", value="Display this help message.", inline=False)
     await interaction.response.send_message(embed=embed)
+
+keep_alive()
+
 bot.run(os.environ.get("DISCORD_BOT_SECRET"))
